6fd9d335f12a0932dca51cccc7cb67c9d2b1874e
Almo Gunadya Sutedjo <t-asutedjo@microsoft.com>
2022-03-16 13:55:43 +0100

    Add Mock SideBar

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,7 +5,14 @@ import {
   ContainerClient,
 } from "@azure/storage-blob";
 import { useEffect, useRef, useState } from "react";
-import { IGroup, Panel } from "@fluentui/react";
+import {
+  IGroup,
+  INavLinkGroup,
+  INavStyles,
+  Nav,
+  Panel,
+  Stack,
+} from "@fluentui/react";
 import AudioPlayer from "./AudioPlayer";
 import {
   convertMilliseconds,
@@ -149,21 +156,105 @@ function App() {
     openPanel();
   };
 
+  const navLinkGroups: INavLinkGroup[] = [
+    {
+      links: [
+        {
+          name: "Home",
+          url: "http://example.com",
+          expandAriaLabel: "Expand Home section",
+          collapseAriaLabel: "Collapse Home section",
+          links: [
+            {
+              name: "Activity",
+              url: "http://msn.com",
+              key: "key1",
+              target: "_blank",
+            },
+            {
+              name: "MSN",
+              url: "http://msn.com",
+              key: "key2",
+              target: "_blank",
+            },
+          ],
+          isExpanded: true,
+        },
+        {
+          name: "Documents",
+          url: "http://example.com",
+          key: "key3",
+          isExpanded: true,
+          target: "_blank",
+        },
+        {
+          name: "Pages",
+          url: "http://msn.com",
+          key: "key4",
+          target: "_blank",
+        },
+        {
+          name: "Notebook",
+          url: "http://msn.com",
+          key: "key5",
+        },
+        {
+          name: "Communication and Media",
+          url: "http://msn.com",
+          key: "key6",
+          target: "_blank",
+        },
+        {
+          name: "News",
+          url: "http://cnn.com",
+          icon: "News",
+          key: "key7",
+          target: "_blank",
+        },
+      ],
+    },
+  ];
+
+  const navStyles: Partial<INavStyles> = {
+    root: {
+      width: 208,
+      height: "100vh",
+      boxSizing: "border-box",
+      border: "1px solid #eee",
+      overflowY: "auto",
+      backgroundColor: "#fafafa",
+    },
+  };
+
   return (
     <div className="App">
       <NavBar />
-      <OptionsBar />
-      <FilterBar />
-      <FilesExplorer showAudioPlayer={showAudioPlayer} blobs={blobs} />
-      <Panel
-        isLightDismiss
-        isOpen={isOpen}
-        onDismiss={dismissPanel}
-        closeButtonAriaLabel="Close"
-        headerText="Audio Player"
-      >
-        <AudioPlayer src={audioUrl} transcript={transcript} />
-      </Panel>
+      <Stack horizontal>
+        <Stack.Item>
+          <Nav
+            // onLinkClick={_onLinkClick}
+            selectedKey="key3"
+            ariaLabel="Nav basic example"
+            styles={navStyles}
+            groups={navLinkGroups}
+          />
+        </Stack.Item>
+        <Stack.Item grow>
+          <OptionsBar />
+          <FilterBar />
+
+          <FilesExplorer showAudioPlayer={showAudioPlayer} blobs={blobs} />
+          <Panel
+            isLightDismiss
+            isOpen={isOpen}
+            onDismiss={dismissPanel}
+            closeButtonAriaLabel="Close"
+            headerText="Audio Player"
+          >
+            <AudioPlayer src={audioUrl} transcript={transcript} />
+          </Panel>
+        </Stack.Item>
+      </Stack>
     </div>
   );
 }
