17f10b2c368a8fb4c2feade839119bde1854f842
Almo Gunadya Sutedjo <t-asutedjo@microsoft.com>
2022-03-22 15:51:09 +0100

    Change Sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,83 +1,18 @@
-import { INavLinkGroup, INavStyles, Nav } from "@fluentui/react";
+import { Icon, SearchBox } from "@fluentui/react";
+import "styles/SideBar.css";
 
 function SideBar() {
-  const navLinkGroups: INavLinkGroup[] = [
-    {
-      links: [
-        {
-          name: "Home",
-          url: "http://example.com",
-          expandAriaLabel: "Expand Home section",
-          collapseAriaLabel: "Collapse Home section",
-          links: [
-            {
-              name: "Activity",
-              url: "http://msn.com",
-              key: "key1",
-              target: "_blank",
-            },
-            {
-              name: "MSN",
-              url: "http://msn.com",
-              key: "key2",
-              target: "_blank",
-            },
-          ],
-          isExpanded: true,
-        },
-        {
-          name: "Documents",
-          url: "http://example.com",
-          key: "key3",
-          isExpanded: true,
-          target: "_blank",
-        },
-        {
-          name: "Pages",
-          url: "http://msn.com",
-          key: "key4",
-          target: "_blank",
-        },
-        {
-          name: "Notebook",
-          url: "http://msn.com",
-          key: "key5",
-        },
-        {
-          name: "Communication and Media",
-          url: "http://msn.com",
-          key: "key6",
-          target: "_blank",
-        },
-        {
-          name: "News",
-          url: "http://cnn.com",
-          icon: "News",
-          key: "key7",
-          target: "_blank",
-        },
-      ],
-    },
-  ];
-
-  const navStyles: Partial<INavStyles> = {
-    root: {
-      width: 208,
-      height: "100vh",
-      boxSizing: "border-box",
-      border: "1px solid #eee",
-      overflowY: "auto",
-      backgroundColor: "#fafafa",
-    },
-  };
   return (
-    <Nav
-      // onLinkClick={_onLinkClick}
-      selectedKey="key3"
-      ariaLabel="Nav basic example"
-      styles={navStyles}
-      groups={navLinkGroups}
-    />
+    <div className="SideBar">
+      <div className="SideBar__Title">Saved Queries</div>
+      <div className="SideBar__SearchBar">
+        <SearchBox placeholder="Search" disabled />
+      </div>
+      <div className="SideBar__QueryTitle">
+        <Icon className="SideBar__QueryIcon" iconName="CaretSolidDown" /> My
+        Queries
+      </div>
+    </div>
   );
 }
 export default SideBar;
