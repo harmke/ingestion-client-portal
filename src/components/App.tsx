29172fddb25f17a928046a2ba50bfe6258ebae56
Almo Gunadya Sutedjo <t-asutedjo@microsoft.com>
2022-04-06 13:26:56 +0200

    Add Audio Pagination

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,7 @@
 import "styles/App.css";
 import { BlobServiceClient, ContainerClient } from "@azure/storage-blob";
 import { useEffect, useRef, useState } from "react";
-import { IGroup, Panel, Stack } from "@fluentui/react";
+import { DefaultButton, Panel, Stack } from "@fluentui/react";
 import AudioPlayer from "./AudioPlayer";
 import { generateTranscript, Transcript } from "../utils/transcription";
 import { useBoolean } from "@fluentui/react-hooks";
@@ -12,16 +12,16 @@ import FilterBar from "./FilterBar";
 import OptionsBar from "./OptionsBar";
 import SideBar from "./SideBar/SideBar";
 import ConnectionStringBar from "./ConnectionStringBar";
-import { Blob, Container, fetchAudioBlobs, getJsonData } from "utils/blobData";
+import { Blob, getJsonData } from "utils/blobData";
+import PaginatedAudio from "utils/audioPagination";
 
 export type LoadingStatus = "pending" | "loading" | "successful" | "failed";
 
 function App() {
-  const [containers, setContainers] = useState<Array<Container>>([]);
   const [blobs, setBlobs] = useState<Array<Blob>>([]);
   const [blobsLoadingStatus, setBlobsLoadingStatus] =
     useState<LoadingStatus>("pending");
-  const [groups, setGroups] = useState<Array<IGroup>>([]);
+  const paginatedAudioRef = useRef<PaginatedAudio>();
 
   const [audioUrl, setAudioUrl] = useState("");
   const [transcript, setTranscript] = useState<Transcript>([]);
@@ -63,21 +63,27 @@ function App() {
       return;
     }
 
+    paginatedAudioRef.current = new PaginatedAudio(
+      blobServiceClientRef.current,
+      "audio-processed",
+      10
+    );
+
     jsonResultOutputContainerClientRef.current =
       blobServiceClientRef.current.getContainerClient("json-result-output");
 
     const setAudioBlobs = async () => {
-      if (!blobServiceClientRef.current) {
+      if (!blobServiceClientRef.current || !paginatedAudioRef.current) {
+        setBlobsLoadingStatus("failed");
         return;
       }
       setBlobsLoadingStatus("loading");
       try {
-        const [newBlobs, newContainers] = await fetchAudioBlobs(
-          blobServiceClientRef.current
-        );
+        const newBlobs = await paginatedAudioRef.current
+          .getNextAudioBlobs()
+          .catch(() => []);
 
         setBlobs(newBlobs);
-        setContainers(newContainers);
         setBlobsLoadingStatus("successful");
       } catch (e) {
         setBlobsLoadingStatus("failed");
@@ -86,27 +92,16 @@ function App() {
     setAudioBlobs();
   }, [blobServiceSas]);
 
-  useEffect(() => {
-    const createGroups = () => {
-      let groups = [];
-      let indexCount = 0;
-
-      for (const { name, size } of containers) {
-        groups.push({
-          key: `group${name}`,
-          name: name,
-          startIndex: indexCount,
-          count: size,
-          level: 0,
-        });
-        indexCount += size;
-      }
-
-      return groups;
-    };
-
-    setGroups(createGroups());
-  }, [containers]);
+  const loadNextAudioBlobPage = async () => {
+    if (!blobServiceClientRef.current || !paginatedAudioRef.current) {
+      setBlobsLoadingStatus("failed");
+      return;
+    }
+    setBlobsLoadingStatus("loading");
+    const newBlobs = await paginatedAudioRef.current.getNextAudioBlobs();
+    setBlobs([...blobs, ...newBlobs]);
+    setBlobsLoadingStatus("successful");
+  };
 
   const showAudioPlayer = async (item: Blob) => {
     setAudioUrl(item.blobClient.url);
@@ -137,21 +132,25 @@ function App() {
             blobs={blobs}
             loadingStatus={blobsLoadingStatus}
           />
-          <Panel
-            isLightDismiss
-            isOpen={isOpen}
-            onDismiss={dismissPanel}
-            closeButtonAriaLabel="Close"
-            headerText="Audio Player"
-          >
-            <AudioPlayer
-              src={audioUrl}
-              transcript={transcript}
-              transcriptLoadingStatus={transcriptLoadingStatus}
-            />
-          </Panel>
+          <div className="PaginationButton__Container">
+            <DefaultButton text="load more" onClick={loadNextAudioBlobPage} />
+          </div>
         </Stack.Item>
       </Stack>
+
+      <Panel
+        isLightDismiss
+        isOpen={isOpen}
+        onDismiss={dismissPanel}
+        closeButtonAriaLabel="Close"
+        headerText="Audio Player"
+      >
+        <AudioPlayer
+          src={audioUrl}
+          transcript={transcript}
+          transcriptLoadingStatus={transcriptLoadingStatus}
+        />
+      </Panel>
     </div>
   );
 }
--- src/utils/blobData.ts
+++ src/utils/blobData.ts
@@ -7,12 +7,6 @@ import {
 import getBlobDuration from "get-blob-duration";
 import { convertMilliseconds } from "./transcription";
 
-export interface Container {
-  name: string;
-  containerClient: ContainerClient;
-  size: number;
-}
-
 export interface Blob {
   name: string;
   createdOn: string | undefined;
@@ -35,7 +29,7 @@ export function getJsonData<T = any>(url: string): Promise<T> {
     });
 }
 
-const handleBlobClient = async (
+export const handleBlobClient = async (
   blob: BlobItem,
   containerClient: ContainerClient
 ): Promise<Blob> => {
@@ -48,58 +42,4 @@ const handleBlobClient = async (
     duration: convertMilliseconds(duration * 1000),
     blobClient: blobClient,
   };
-};
-
-const handleContainerClient = async (
-  containerName: string,
-  blobServiceClient: BlobServiceClient
-): Promise<[Blob[], Container]> => {
-  const containerClient = blobServiceClient.getContainerClient(containerName);
-
-  const blobRequests = [];
-  for await (const response of containerClient
-    .listBlobsFlat()
-    .byPage({ maxPageSize: 2 })) {
-    console.log(response);
-    for await (const blob of response.segment.blobItems) {
-      if ([".wav", ".mp3"].some((suffix) => blob.name.endsWith(suffix)))
-        blobRequests.push(handleBlobClient(blob, containerClient));
-    }
-  }
-  const resolvedBlobRequests = await Promise.all(blobRequests);
-
-  const container: Container = {
-    name: containerName,
-    containerClient: containerClient,
-    size: resolvedBlobRequests.length,
-  };
-
-  return [resolvedBlobRequests, container];
-};
-
-export const fetchAudioBlobs = async (
-  blobServiceClient: BlobServiceClient
-): Promise<[Blob[], Container[]]> => {
-  const containerRequests = [];
-  for await (const container of blobServiceClient.listContainers({
-    prefix: "audio",
-  })) {
-    containerRequests.push(
-      handleContainerClient(container.name, blobServiceClient)
-    );
-  }
-
-  const resolvedContainerRequests = await Promise.all(containerRequests);
-
-  return [
-    resolvedContainerRequests.map((el) => el[0]).flat(),
-    resolvedContainerRequests.map((el) => el[1]).flat(),
-  ];
-};
-
-export const fetchContainerAudioBlobData = async (
-  containerName: string = "audio-processed",
-  blobServiceClient: BlobServiceClient
-): Promise<[Blob[], Container]> => {
-  return await handleContainerClient(containerName, blobServiceClient);
 };
