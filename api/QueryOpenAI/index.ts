86777e17cad676d056bb5983a68957e2d8514844
Harmke Alkemade <harmkealkemade@gmail.com>
2023-02-10 12:25:25 +0100

    Returning text from api

--- api/QueryOpenAI/index.ts
+++ api/QueryOpenAI/index.ts
@@ -4,10 +4,10 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     context.log('HTTP trigger function processed a request.');
     const prompt = (req.query.prompt || (req.body && req.body.prompt));
 
-    // const api_key = process.env.API_KEY;
-    // const base_url =  process.env.AOAI_ENDPOINT;
-    // const deployment_name = process.env.DEPLOYMENT_NAME;
-    // const url = base_url + "/openai/deployments/" + deployment_name + "/completions?api-version=2022-12-01";
+    const api_key = process.env.API_KEY;
+    const base_url =  process.env.AOAI_ENDPOINT;
+    const deployment_name = process.env.DEPLOYMENT_NAME;
+    const url = base_url + "/openai/deployments/" + deployment_name + "/completions?api-version=2022-12-01";
 
     // let output = '';
     // const response = await fetch(url, {
@@ -38,7 +38,7 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
 
 
     context.res.json({
-        text: prompt
+        text: url
     });
 
 };
