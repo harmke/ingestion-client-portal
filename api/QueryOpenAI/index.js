ec6140f447781e3657ab65ad85834eecd1a513df
Harmke Alkemade <harmkealkemade@gmail.com>
2023-02-10 20:14:52 +0100

    Update index.js

--- api/QueryOpenAI/index.js
+++ api/QueryOpenAI/index.js
@@ -13,16 +13,40 @@ module.exports = async function (context, req) {
     // const url = base_url + "/openai/deployments/" + deployment_name + "/completions?api-version=2022-12-01";
     const url = "https://sam-openai-rs.openai.azure.com/openai/deployments/text-davinci-003/completions?api-version=2022-12-01";
 
+    try {
+        const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "api-key": api_key,
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            "temperature": 0,
+            "max_tokens":600,
+            "prompt" : "say test",
+            })
+        })
+
+        const result = await response.json()
+        context.res = result
+
+    } catch (error) {
+        context.res = {
+            error: error.toString()
+        };
+    }
+
 
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+    // context.res = {
+    //     // status: 200, /* Defaults to 200 */
+    //     body: responseMessage
+    // };
 }
 
 
