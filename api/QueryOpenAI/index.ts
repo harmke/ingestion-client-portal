325ff45672bf94a430e6cdcc94a8ba4735c6c369
Harmke Alkemade <harmkealkemade@gmail.com>
2023-02-10 12:20:12 +0100

    Update index.ts

--- api/QueryOpenAI/index.ts
+++ api/QueryOpenAI/index.ts
@@ -4,25 +4,25 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     context.log('HTTP trigger function processed a request.');
     const prompt = (req.query.prompt || (req.body && req.body.prompt));
 
-    const api_key = process.env.API_KEY;
-    const base_url =  process.env.AOAI_ENDPOINT;
-    const deployment_name = process.env.DEPLOYMENT_NAME;
-    const url = base_url + "/openai/deployments/" + deployment_name + "/completions?api-version=2022-12-01";
+    // const api_key = process.env.API_KEY;
+    // const base_url =  process.env.AOAI_ENDPOINT;
+    // const deployment_name = process.env.DEPLOYMENT_NAME;
+    // const url = base_url + "/openai/deployments/" + deployment_name + "/completions?api-version=2022-12-01";
 
-    let output = '';
-    const response = await fetch(url, {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json",
-        "api-key": api_key as string,
-        Accept: "application/json",
-    },
-    body:    JSON.stringify(   {
-        "temperature": 0,
-        "max_tokens":600,
-        "prompt" : prompt
-        })
-    })
+    // let output = '';
+    // const response = await fetch(url, {
+    // method: "POST",
+    // headers: {
+    //     "Content-Type": "application/json",
+    //     "api-key": api_key as string,
+    //     Accept: "application/json",
+    // },
+    // body:    JSON.stringify(   {
+    //     "temperature": 0,
+    //     "max_tokens":600,
+    //     "prompt" : prompt
+    //     })
+    // })
     // .then(function(response){ 
     //     return response.json()})
     //     .then(function(data)
