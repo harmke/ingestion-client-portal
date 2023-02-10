7819c14ef66c77a696f35f0dd3cc8e4a93a8360a
Harmke Alkemade <harmkealkemade@gmail.com>
2023-02-10 20:01:36 +0100

    dummy

--- api/QueryOpenAI/index.js
+++ api/QueryOpenAI/index.js
@@ -1,43 +1,57 @@
-// import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+// const { AzureFunction, Context, HttpRequest } = require("@azure/functions")
 
 // import fetch from "node-fetch"
 // const fetch = require("node-fetch");
 // const querystring = require("querystring");
 
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
 
-const httpTrigger = async function (context, req) {
-    context.log('HTTP trigger function processed a request.');
-    // const prompt = (req.query.prompt || (req.body && req.body.prompt));
+    const name = (req.query.name || (req.body && req.body.name));
+    const responseMessage = name
+        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
-    const api_key = process.env.API_KEY;
-    const base_url =  process.env.AOAI_ENDPOINT;
-    const deployment_name = process.env.DEPLOYMENT_NAME;
-    // const url = base_url + "/openai/deployments/" + deployment_name + "/completions?api-version=2022-12-01";
-    const url = "https://sam-openai-rs.openai.azure.com/openai/deployments/text-davinci-003/completions?api-version=2022-12-01";
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+}
 
-    try {
-        const response = await fetch(url, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "api-key": api_key,
-            Accept: "application/json",
-        },
-        body: JSON.stringify({
-            "temperature": 0,
-            "max_tokens":600,
-            "prompt" : "say test",
-            })
-        })
 
-        const result = await response.json()
-        context.res.json(result)
+// const httpTrigger = async function (context, req) {
+//     context.log('HTTP trigger function processed a request.');
+//     // const prompt = (req.query.prompt || (req.body && req.body.prompt));
 
-    } catch (error) {
-        context.res.json({
-        error: error.toString()
-    });
-    }
-};
+//     const api_key = process.env.API_KEY;
+//     const base_url =  process.env.AOAI_ENDPOINT;
+//     const deployment_name = process.env.DEPLOYMENT_NAME;
+//     // const url = base_url + "/openai/deployments/" + deployment_name + "/completions?api-version=2022-12-01";
+//     const url = "https://sam-openai-rs.openai.azure.com/openai/deployments/text-davinci-003/completions?api-version=2022-12-01";
 
-export default httpTrigger;
+//     try {
+//         const response = await fetch(url, {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json",
+//             "api-key": api_key,
+//             Accept: "application/json",
+//         },
+//         body: JSON.stringify({
+//             "temperature": 0,
+//             "max_tokens":600,
+//             "prompt" : "say test",
+//             })
+//         })
+
+//         const result = await response.json()
+//         context.res.json(result)
+
+//     } catch (error) {
+//         context.res.json({
+//         error: error.toString()
+//     });
+//     }
+// };
+
+// module.exports = httpTrigger;
