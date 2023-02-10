08f40e3e6873a2374f290c7dad0b28febd14db3b
Harmke Alkemade <harmkealkemade@gmail.com>
2023-02-10 18:44:02 +0100

    Update index.js

--- api/QueryOpenAI/index.js
+++ api/QueryOpenAI/index.js
@@ -1,33 +1,43 @@
-const fetch = require('node-fetch');
+const { AzureFunction, Context, HttpRequest } = require("@azure/functions")
 
-module.exports = async function (context, req) {
-  context.log('HTTP trigger function processed a request.');
-  // const prompt = (req.query.prompt || (req.body && req.body.prompt));
+// import fetch from "node-fetch"
+// const fetch = require("node-fetch");
+// const querystring = require("querystring");
 
-  const api_key = "04e8a255f5984800928bd8b42c129d8c";
-  const url = "https://sam-openai-rs.openai.azure.com/openai/deployments/text-davinci-003/completions?api-version=2022-12-01";
 
-  try {
-      const response = await fetch(url, {
-      method: "POST",
-      headers: {
-          "Content-Type": "application/json",
-          "api-key": api_key,
-          Accept: "application/json",
-      },
-      body: JSON.stringify({
-          "temperature": 0,
-          "max_tokens":600,
-          "prompt" : "say test",
-          })
-      })
+const httpTrigger = async function (context, req) {
+    context.log('HTTP trigger function processed a request.');
+    // const prompt = (req.query.prompt || (req.body && req.body.prompt));
 
-      const result = await response.json()
-      context.res.json(result)
+    const api_key = process.env.API_KEY;
+    const base_url =  process.env.AOAI_ENDPOINT;
+    const deployment_name = process.env.DEPLOYMENT_NAME;
+    // const url = base_url + "/openai/deployments/" + deployment_name + "/completions?api-version=2022-12-01";
+    const url = "https://sam-openai-rs.openai.azure.com/openai/deployments/text-davinci-003/completions?api-version=2022-12-01";
 
-  } catch (error) {
-      context.res.json({
-      error: error.toString()
-  });
-  }
+    try {
+        const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "api-key": api_key,
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            "temperature": 0,
+            "max_tokens":600,
+            "prompt" : "say test",
+            })
+        })
+
+        const result = await response.json()
+        context.res.json(result)
+
+    } catch (error) {
+        context.res.json({
+        error: error.toString()
+    });
+    }
 };
+
+module.exports = httpTrigger;
