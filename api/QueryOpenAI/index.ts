599d60f33b0d5cb56416c9dbe5484b47a778afce
Harmke Alkemade <harmkealkemade@gmail.com>
2023-02-10 14:28:55 +0100

    Update index.ts

--- api/QueryOpenAI/index.ts
+++ api/QueryOpenAI/index.ts
@@ -1,5 +1,8 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
-import fetch from "node-fetch"
+// import fetch from "node-fetch"
+const https = require("https");
+const querystring = require("querystring");
+
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
@@ -11,22 +14,58 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     const url = base_url + "/openai/deployments/" + deployment_name + "/completions?api-version=2022-12-01";
 
     try {
-        const response = await fetch(url, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "api-key": api_key as string,
-            Accept: "application/json",
-        },
-        body: JSON.stringify({
-            "temperature": 0,
-            "max_tokens":600,
-            "prompt" : "say test"
-            })
-        })
+        // const response = await fetch(url, {
+        // method: "POST",
+        // headers: {
+        //     "Content-Type": "application/json",
+        //     "api-key": api_key as string,
+        //     Accept: "application/json",
+        // },
+        // body: JSON.stringify({
+        //     "temperature": 0,
+        //     "max_tokens":600,
+        //     "prompt" : "say test",
+        //     })
+        // })
 
-        const result = await response.json()
-        context.res.json(result)
+        // const result = await response.json()
+        // context.res.json(result)
+
+        const data = querystring.stringify({
+            temperature: 0,
+            max_tokens: 600,
+            prompt: "say test"
+          });
+          
+          const options = {
+            hostname: new URL(url).hostname,
+            path: new URL(url).pathname,
+            method: "POST",
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded",
+              "Content-Length": data.length,
+              "api-key": api_key,
+              Accept: "application/json"
+            }
+          };
+          
+          const req = https.request(options, res => {
+            res.setEncoding("utf8");
+            let response = "";
+          
+            res.on("data", chunk => {
+              response += chunk;
+            });
+          
+            res.on("end", () => {
+              console.log(response);
+              context.res.json(response);
+            });
+          });
+          
+          req.write(data);
+          req.end();
+
     } catch (error) {
         context.res.json({
         error: error
