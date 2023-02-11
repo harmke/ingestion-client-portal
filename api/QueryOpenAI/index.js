bde20eeea9251129920080a92da7fbbc305632b0
Harmke Alkemade <harmkealkemade@gmail.com>
2023-02-11 09:09:01 +0100

    Update index.js

--- api/QueryOpenAI/index.js
+++ api/QueryOpenAI/index.js
@@ -6,12 +6,12 @@
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
+    const prompt = (req.query.prompt || (req.body && req.body.prompt));
 
     const api_key = process.env.API_KEY;
     const base_url =  process.env.AOAI_ENDPOINT;
     const deployment_name = process.env.DEPLOYMENT_NAME;
-    // const url = base_url + "/openai/deployments/" + deployment_name + "/completions?api-version=2022-12-01";
-    const url = "https://sam-openai-rs.openai.azure.com/openai/deployments/text-davinci-003/completions?api-version=2022-12-01";
+    const url = base_url + "/openai/deployments/" + deployment_name + "/completions?api-version=2022-12-01";
 
     try {
         const response = await fetch(url, {
@@ -24,34 +24,21 @@ module.exports = async function (context, req) {
         body: JSON.stringify({
             "temperature": 0,
             "max_tokens":600,
-            "prompt" : "say test",
+            "prompt" : prompt,
             })
         })
 
         const result = await response.json()
         const text = result.choices[0].text
         context.res.json({
-                // status: 200, /* Defaults to 200 */
             text: text
         });
-        // context.res.json(result)
 
     } catch (error) {
         context.res.json({
             error: error.toString()
         });
     }
-
-
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
-    // context.res = {
-    //     // status: 200, /* Defaults to 200 */
-    //     body: responseMessage
-    // };
 }
 
 
